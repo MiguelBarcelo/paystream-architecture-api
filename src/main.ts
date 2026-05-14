76f5e8f0fefb7e3b4fc9bee6a1fb8aef425547b1
Miguel Barceló <miguel.barcelo@gmail.com>
2026-05-14 15:06:54 -0700

    feat: Add useGlobalPipes
    Convert BigInt to String at response

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+
+// TODO: Use SerlializerInterceptor
+(BigInt.prototype as any).toJSON = function () {
+  return this.toString();
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Apply the validation pipe globally
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Remove properties not defined in the DTO
+      forbidNonWhitelisted: true, // Throw an error if non-whitelisted properties are present
+      transform: true, // Automatically transform payloads to match DTO types
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
