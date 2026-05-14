2076f6d3e566d93928cf9bc63eabc838564416c7
Miguel Barceló <miguel.barcelo@gmail.com>
2026-05-14 15:38:58 -0700

    feat: Add Payments table

---
+++ prisma/migrations/20260513215821_add_payments_table/migration.sql
@@ -0,0 +1,22 @@
+-- CreateEnum
+CREATE TYPE "payment_status" AS ENUM ('PENDING', 'PROCESSING', 'COMPLETED', 'FAILED');
+
+-- CreateTable
+CREATE TABLE "payments" (
+    "id" SERIAL NOT NULL,
+    "idempotency_key" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "amount_in_cents" BIGINT NOT NULL,
+    "currency" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "last4" VARCHAR(4),
+    "store_id" INTEGER NOT NULL,
+    "status" "payment_status" NOT NULL DEFAULT 'PENDING',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "payments_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "payments_idempotency_key_key" ON "payments"("idempotency_key");
