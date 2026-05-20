8a1f1f7475215aadd422726207e6918e55a30e88
Miguel Barceló <miguel.barcelo@gmail.com>
2026-05-19 17:40:51 -0700

    feat: Register RabbitMQ Client
    in PaymentsModule

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -2,10 +2,31 @@ import { PrismaModule } from 'src/prisma/prisma.module';
 import { Module } from '@nestjs/common';
 import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    PrismaModule,
+    // Configure the client to communicate with RabbitMQ
+    ClientsModule.registerAsync([
+      {
+        name: 'PAYMENT_STREAM',
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            urls: [configService.get<string>('RABBITMQ_URL') as string],
+            queue: configService.get<string>('RMQ_QUEUE') as string,
+            queueOptions: {
+              durable: true,
+            },
+          },
+        }),
+        inject: [ConfigService],
+      },
+    ]),
+  ],
   controllers: [PaymentsController],
   providers: [PaymentsService],
-  imports: [PrismaModule],
 })
 export class PaymentsModule {}
