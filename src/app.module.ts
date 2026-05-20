4b90b5eaf79d2d08697bfa82d65a256748a01f02
Miguel Barceló <miguel.barcelo@gmail.com>
2026-05-19 17:40:40 -0700

    feat: Register ScheduleModule
    in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { AppService } from './app.service';
 import { PaymentsModule } from './payments/payments.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [ConfigModule.forRoot(), PaymentsModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PaymentsModule,
+    PrismaModule,
+    ScheduleModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
