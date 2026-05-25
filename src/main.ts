690ae3fb115a329d8c02f93cd10d06f1478b5ab7
Miguel Barceló <miguel.barcelo@gmail.com>
2026-05-24 17:20:56 -0700

    feat: Add Response Listener at API HTTP

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
 
 // TODO: Use SerlializerInterceptor
 (BigInt.prototype as any).toJSON = function () {
@@ -8,6 +10,9 @@ import { AppModule } from './app.module';
 };
 
 async function bootstrap() {
+  const appContext = await NestFactory.createApplicationContext(AppModule);
+  const configService = appContext.get(ConfigService);
+
   const app = await NestFactory.create(AppModule);
 
   // Apply the validation pipe globally
@@ -19,6 +24,20 @@ async function bootstrap() {
     }),
   );
 
+  // Connect the hybrid microservice to listen the responses
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [configService.get<string>('RABBITMQ_URL') as string],
+      queue: configService.get<string>('RMQ_REPLY_QUEUE'),
+      noAck: true,
+    },
+  });
+
+  await appContext.close();
+
+  await app.startAllMicroservices();
   await app.listen(process.env.PORT ?? 3000);
+  console.log('HTTP API and Response listener running in parallel');
 }
 bootstrap();
